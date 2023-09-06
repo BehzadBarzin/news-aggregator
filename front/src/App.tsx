9a4a9fc85f37b573da36027b51dcce546ed37cd3
Behzad Barzin <Behzad_Barzin@live.com>
2023-09-07 00:25:24 +0330

    Home: Scaffold

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,27 @@
+import styles from "./constants/styles";
 
 function App() {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <div>Hello World!</div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          NavBar
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>      
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Item
+          Item
+          Item
+          Item
+          Item
+        </div>
+      </div>
     </div>
   )
 }
