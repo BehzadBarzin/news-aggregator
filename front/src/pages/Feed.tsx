efbd2593bc4607a5b0bde61698caa0a6e2b85266
Behzad Barzin <Behzad_Barzin@live.com>
2023-09-07 15:27:02 +0330

    Add keyword search

--- front/src/pages/Feed.tsx
+++ front/src/pages/Feed.tsx
@@ -3,6 +3,7 @@ import { API_URL } from "../constants/Constants";
 import axios from "axios";
 import Article, { TArticle } from "../components/feed/Article";
 import { useIsAuthenticated, useAuthHeader } from "react-auth-kit";
+import styles from "../constants/styles";
 
 function getEnumKeyByEnumValue(myEnum, enumValue) {
     const keys = Object.keys(myEnum).filter(x => myEnum[x] == enumValue);
@@ -151,6 +152,31 @@ function Feed() {
         return filters.authors?.includes(`${id}`) || false;
     }
 
+
+    const [keywordInputValue, setKeywordInputValue] = useState<string>('');
+
+    const onKeywordInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setKeywordInputValue(e.target.value);
+    }
+
+    const addKeyword = (kw: string) => {
+        setFilters((prev) => {
+            return {
+                ...prev,
+                keywords: prev.keywords ? [...prev.keywords, kw] : [kw]
+            }
+        });
+    }
+
+    const removeKeyword = (kw: string) => {
+        setFilters((prev) => {
+            return {
+                ...prev,
+                keywords: prev.keywords?.filter((s) => s != kw)
+            }
+        });
+    }
+
     const isAuthenticated = useIsAuthenticated();
     const authHeader = useAuthHeader()
 
@@ -249,6 +275,46 @@ return (
                     }
                 </li>
         </ul>
+        {/* Keywords */}
+        <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-700">
+        <li className="flex flex-col items-start p-2 text-white group">
+        <h4 className="mb-5">
+            Keywords: 
+        </h4>
+        <div className="max-w-lg">
+            <div className="relative">
+                <input className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-2 leading-tight focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500" placeholder="Enter some tags" 
+                    onKeyUp={(e) => {
+                        if (e.key === 'Enter') addKeyword(keywordInputValue);
+                    }}
+                    onChange={onKeywordInputChange}
+                    value={keywordInputValue}
+                />
+                <div className="hidden">
+                    <div className="absolute z-40 left-0 mt-2 w-full">
+                        <div className="py-1 text-sm bg-white rounded shadow-lg border border-gray-300">
+                        <a className="block py-1 px-5 cursor-pointer hover:bg-indigo-600 hover:text-white">Add tag "<span className="font-semibold" x-text="textInput"></span>"</a>
+                    </div>
+                </div>
+                </div>
+                {/* Keywords in use */}
+                {
+                    filters.keywords?.map((f) => {
+                        return (
+                            <div className="bg-blue-600 inline-flex items-center text-sm rounded mt-2 mr-1 overflow-hidden">
+                                <span className="ml-2 mr-1 leading-relaxed truncate max-w-xs px-1" x-text="tag">{f}</span>
+                                <button className="w-6 h-8 inline-block align-middle text-white bg-blue-400 focus:outline-none"
+                                    onClick={() => { removeKeyword(f) }}>
+                                    <svg className="w-6 h-6 fill-current mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"/></svg>
+                                </button>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </div>
+        </li>
+        </ul>
         {/* Authors */}
         <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-700">
             <li className="flex flex-col items-start p-2 text-white group">
@@ -301,6 +367,15 @@ return (
                     })
                 }
             </div>
+                {
+                    articles.length === 0 ?
+                    <div className={`${styles.flexCenter}`}> 
+                        <h1 className="font-poppins text-2xl font-bold text-gradient mb-6">
+                            No Articles
+                        </h1>
+                    </div>
+                    : null
+                }
         </div>
         </div>
     </div>
